93ad2214a3590539577bba4fd0e919b4b30a3fad
Stiliyan97 <stiliyan199713@gmail.com>
2024-10-18 22:15:22 +0300

    complete the java script exercises 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(cTemp) {
+  let convertedFTemp = (cTemp - 32) * 5/9;
+  if (Number.isInteger(convertedFTemp)){
+    return convertedFTemp;
+  } else {
+    return parseFloat(convertedFTemp.toFixed(1));
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fTemp) {
+  let convertedCTemp = (fTemp * (9/5)) + 32;
+
+  if (Number.isInteger(convertedCTemp)){
+    return convertedCTemp;
+  } else {
+    return parseFloat(convertedCTemp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
