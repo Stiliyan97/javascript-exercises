7d01987122241c3838b2ec8aeec129c4fc545d19
Stiliyan97 <stiliyan199713@gmail.com>
2024-10-18 21:44:46 +0300

    complete the java script exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    let sum = 0;
+    if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt) || secondInt < 0 || firstInt < 0){
+        return "ERROR";
+    }
+    else if (firstInt > secondInt){
+        for (let i = firstInt; i >= secondInt ; i--) {
+            sum += i;
+        }
+    } else {
+    for (let i = firstInt; i <= secondInt ; i++) {
+        sum += i;
+    }
+    }
+    return sum;
 };
 
 // Do not edit below this line
