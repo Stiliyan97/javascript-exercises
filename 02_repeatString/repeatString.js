4002f95279d73f41e573e87e8dc029851c9f2259
Stiliyan97 <stiliyan199713@gmail.com>
2024-10-18 20:52:03 +0300

    complete the java script exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatString, numberOfRepetitions) {
+    let finalString = "";
+    if (numberOfRepetitions < 0) {
+        return "ERROR";
+    }
+    for (let i = 1; i <= numberOfRepetitions; i++) {
+        finalString += repeatString;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
